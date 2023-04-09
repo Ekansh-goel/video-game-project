8336ed1329fe474f65030e118baad22ddc33cbc9
Ekansh Goel <ekanshgoel123@gmail.com>
2023-04-09 18:42:13 -0400

    validation updates

--- js/validation.js
+++ js/validation.js
@@ -95,95 +95,100 @@ $('input[name=firstname]').on('input',function () {
     let firstname = getDiv("firstname").value
     let firstnameError = getDiv("firstnameError")
     if (isStringInValid(firstname)) {
-        // check weather input field is empty or not
-        firstnameError.innerHTML = "Please enter firstname";
+        // check if input field is empty or not
+        firstnameError.innerHTML = "Please enter first name";
         return true
-    }else if(!/^[a-zA-Z ]+$/.test(firstname)){
-        // check using regex wheather firstname firstname is character or only
-        firstnameError.innerHTML = "firstname must contains character only";
+    } else if (!/^[a-zA-Z]+$/.test(firstname)) {
+        // check using regex whether firstname contains only letters
+        firstnameError.innerHTML = "First name must contain only letters";
         return true
-    }else{
+    } else {
         firstnameError.innerHTML = ""
         return false
     }
 })
-$('input[name=lastname]').on('input',function () {
-    let lastname = getDiv("lastname").value
-    let lastnameError = getDiv("lastnameError")
+
+$('input[name=lastname]').on('input', function() {
+    let lastname = getDiv("lastname").value;
+    let lastnameError = getDiv("lastnameError");
+  
     if (isStringInValid(lastname)) {
-        // check weather input field is empty or not
-        lastnameError.innerHTML = "Please enter lastname";
-        return true
-    }else if(!/^[a-zA-Z ]+$/.test(lastname)){
-        // check using regex wheather lastname lastname is character or only
-        lastnameError.innerHTML = "lastname must contains character only";
-        return true
-    }else{
-        lastnameError.innerHTML = ""
-        return false
+      lastnameError.innerHTML = "Please enter your last name";
+      return true;
+    } else if (!/^[a-zA-Z]+$/.test(lastname)) {
+      lastnameError.innerHTML = "Last name must contain letters only";
+      return true;
+    } else {
+      lastnameError.innerHTML = "";
+      return false;
     }
-})
+  });
+  
 $('input[name=email]').on('input',function () {
     let email = getDiv("email").value
     let emailError = getDiv("emailError")
     if (isStringInValid(email)) {
-        // check weather input field is empty or not
+        // check whether input field is empty or not
         emailError.innerHTML = "Please enter email";
         return true
-    }else if(!/^[a-zA-Z ]+$/.test(email)){
-        // check using regex wheather email email is character or only
-        emailError.innerHTML = "email must contains character only";
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+        // check using regex whether email is in a valid format
+        emailError.innerHTML = "Please enter a valid email address";
         return true
-    }else{
+    } else {
         emailError.innerHTML = ""
         return false
     }
 })
+
 $('input[name=birthday]').on('input',function () {
     let birthday = getDiv("birthday").value
     let birthdayError = getDiv("birthdayError")
     if (isStringInValid(birthday)) {
-        // check weather input field is empty or not
+        // check if input field is empty or not
         birthdayError.innerHTML = "Please enter birthday";
         return true
-    }else if(!/^[a-zA-Z ]+$/.test(birthday)){
-        // check using regex wheather birthday birthday is character or only
-        birthdayError.innerHTML = "birthday must contains character only";
-        return true
-    }else{
-        birthdayError.innerHTML = ""
-        return false
+    } else {
+        // check if entered value is a valid date
+        let dateRegex = /^\d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$/;
+        if (!dateRegex.test(birthday)) {
+            birthdayError.innerHTML = "Please enter a valid date in YYYY-MM-DD format";
+            return true;
+        } else {
+            let birthDate = new Date(birthday);
+            let ageInMilliseconds = Date.now() - birthDate.getTime();
+            let ageDate = new Date(ageInMilliseconds);
+            let age = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+            if (age < 18) {
+                birthdayError.innerHTML = "You must be at least 18 years old to register";
+                return true;
+            } else {
+                birthdayError.innerHTML = "";
+                return false;
+            }
+        }
     }
-})
-$('input[name=username]').on('input',function () {
+});
+
+
+$('input[name=username]').on('input', function () {
     let username = getDiv("username").value
     let usernameError = getDiv("usernameError")
     if (isStringInValid(username)) {
-        // check weather input field is empty or not
+        // check if input field is empty or not
         usernameError.innerHTML = "Please enter username";
         return true
-    }else if(username.length<5){
-        // check using regex wheather username is character or only
-        usernameError.innerHTML = "username must contains 5 character";
+    } else if (username.length < 5) {
+        // check if username contains at least 5 characters
+        usernameError.innerHTML = "Username must be at least 5 characters";
         return true
-    }else{
+    } else if (!/^[a-zA-Z0-9_]+$/.test(username)) {
+        // check if username contains only alphanumeric characters and underscore
+        usernameError.innerHTML = "Username must contain only letters, numbers, and underscore";
+        return true
+    } else {
         usernameError.innerHTML = ""
-        return false
-    }
-})
-$('input[name=password]').on('input',function () {
-    let password = getDiv("password").value
-    let passwordError = getDiv("passwordError")
-    if (isStringInValid(password)) {
-        // check weather input field is empty or not
-        passwordError.innerHTML = "Please enter password";
-        return true
-    }else if(!/^[a-zA-Z ]+$/.test(password)){
-        // check using regex wheather password is character or only
-        passwordError.innerHTML = "password must contains character only";
-        return true
-    }else{
-        passwordError.innerHTML = ""
         return false
     }
 })
@@ -192,22 +197,24 @@ $('input[name=repassword]').on('input',function () {
     let password = getDiv("password").value
     let repasswordError = getDiv("repasswordError")
     if (isStringInValid(repassword)) {
-        // check weather input field is empty or not
         repasswordError.innerHTML = "Please enter repassword";
-        return true
-    }else if (password !=repassword){
-        repasswordError.innerHTML = "password doesnot match";
-        return true
-    }
-    else if(!/^[a-zA-Z ]+$/.test(repassword)){
-        // check using regex wheather repassword is character or only
-        repasswordError.innerHTML = "repassword must contains character only";
-        return true
-    }else{
-        repasswordError.innerHTML = ""
-        return false
+        return true;
+    } else if (password != repassword) {
+        repasswordError.innerHTML = "Passwords do not match";
+        return true;
+    } else if (!/^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/.test(password)) {
+        repasswordError.innerHTML = "Password must contain at least one letter, one number and one special character";
+        return true;
+    } else if (password.length < 8) {
+        repasswordError.innerHTML = "Password must be at least 8 characters long";
+        return true;
+    } else {
+        repasswordError.innerHTML = "";
+        return false;
     }
 })
+
+
 // check string is empty or not
 let isStringInValid = (string) => {
     return !string || !string?.trim()
